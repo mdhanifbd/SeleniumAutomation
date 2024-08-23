f60b86409309b6937a14cf892bb99d04aa9b7d52
hanifsqabd <hanifsqabd@gmail.com>
2024-08-23 23:23:00 +0600

    commit new code

--- src/test/java/seleniumAutomation/SeleniumAutomation2.java
+++ src/test/java/seleniumAutomation/SeleniumAutomation2.java
@@ -3,7 +3,7 @@ package seleniumAutomation;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumAutomation {
+public class SeleniumAutomation2 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.get("https://parabank.parasoft.com/parabank/services.htm");
